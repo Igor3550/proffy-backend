1868ea34ac0d7422c853bac2175ca750e7a905db
Igor Melo <igormeloigormelo@gmail.com>
2020-09-14 16:21:52 -0300

    new update

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/proffy', { useMongoClient: true })
+mongoose.connect(process.env.MONGO_URL, { useMongoClient: true })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
